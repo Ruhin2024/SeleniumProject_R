234ee0fdb6435d6fc2e5bea8b1f5fe01326360bd
Ruhin2024 <valiyevruhin777@gmail.com>
2025-03-30 15:50:00 +0200

    SLP0302_R

---
+++ src/test/java/day03_drivermethods_implicitlywait_junit/Practice02_R.java
@@ -0,0 +1,40 @@
+package day03_drivermethods_implicitlywait_junit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Practice02_R {
+    public static void main(String[] args) throws InterruptedException {
+
+        //Go to TechPro page. https://www.techproeducation.com/
+        //Print the page position and size
+        //Minimize the page
+        //Wait 3 seconds in minimized mode and maximize the page
+        //Print the page position and size in maximized mode
+        //Make the page fullscreen
+        //Print the page position and size in fullscreen mode
+        //Close the page
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.techproeducation.com/");
+        System.out.println("Sayfanin yeri =>" + driver.manage().window().getPosition());
+        System.out.println("Sayfanin boyukluyu =>" + driver.manage().window().getSize());
+        //-----------------------------
+        driver.manage().window().minimize();
+       Thread.sleep(15000);
+       System.out.println("Sayfanin yeri =>" + driver.manage().window().getPosition());
+       System.out.println("Sayfanin boyukluyu =>" + driver.manage().window().getSize());
+       //---------------------------------
+       driver.manage().window().fullscreen();
+       Thread.sleep(15000);
+       System.out.println("Sayfanin yeri =>" + driver.manage().window().getPosition());
+       System.out.println("Sayfanin boyukluyu =>" + driver.manage().window().getSize());
+
+       Thread.sleep(15000);
+       driver.quit();
+
+
+
+    }
+}
